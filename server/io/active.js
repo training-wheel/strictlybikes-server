9ea5bb3a13615dcf578a66191f0346e0f5671740
AS Wilson <alexander.wilson@ncf.edu>
2019-08-08 20:18:35 -0500

    Init module socket events for active games

---
+++ server/io/active.js
@@ -0,0 +1,20 @@
+const { games, userGames } = require('../../db/index').models; 
+
+const createGame = (data) => {
+  //  create new game
+  const gameOptions = {
+
+  }
+};
+
+class ActiveSocket {
+  constructor(socket, server) {
+    this.server = server;
+    this.socket = socket;
+    this.handlers = {
+      createGame: createGame.bind(this);
+    };
+  }
+};
+
+module.exports.ActiveSocket = ActiveSocket;
