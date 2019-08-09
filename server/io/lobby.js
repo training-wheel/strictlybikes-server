d445d06bc9cdce6978451eb944455c6cbfc374fd
AS Wilson <alexander.wilson@ncf.edu>
2019-08-08 20:17:19 -0500

    Implement joinGame socket event

--- server/io/lobby.js
+++ server/io/lobby.js
@@ -1,15 +1,30 @@
-const newGame = () => {
-  //  insert code
-};
+const { usergames, games } = require('../../db/index').models;
+
 
 class LobbySocket {
   constructor(socket, server) {
     this.server = server;
     this.socket = socket;
     this.handlers = {
-      newGame: newGame.bind(this),
+      joinGame: async (data) => {
+        try {
+          const { room, userId } = data;
+          const game = await games.findOne({
+            where: {
+              code: room,
+            },
+          });
+          const { id: gameId } = game;
+          await usergames.create({ userId, gameId });
+          socket.join(room);
+          socket.to(room).emit('join', `Congratulations you joined ${room}`);
+          socket.to('lobby').emit('newGame', JSON.stringify(game));
+        } catch (err) {
+          console.error(`Failed to join room: ${err}`);
+        }
+      },
     };
   }
-}
+};
 
-module.exports.LobbySocket = LobbySocket;
+exports.LobbySocket = LobbySocket;
