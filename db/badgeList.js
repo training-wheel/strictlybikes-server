4601bbb2e7b99b19f788fda84def5ae73deba5cf
Operation Spark <miguelpeter2@gmail.com>
2019-08-12 18:15:54 -0500

    Server autopopulates badges and metrics tables in database

---
+++ db/badgeList.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: '5WinBadge',
+    description: 'Win 5 games of Scatter!',
+    goal: 5,
+  },
+
+  {
+    name: '10WinBadge',
+    description: 'Win 10 games of Scatter!',
+    goal: 10,
+  },
+  {
+    name: '15WinBadge',
+    description: 'Win 15 games of Scatter!',
+    goal: 15,
+  },
+];
---
+++ db/userMetrics.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'Kilometers Traveled',
+  },
+  {
+    name: 'Top Speed',
+  },
+  {
+    name: 'Wins',
+  },
+  {
+    name: 'Games',
+  },
+  {
+    name: 'Losses',
+  },
+  {
+    name: 'Time Playing',
+  },
+];
