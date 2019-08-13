0516d14f6033949ac4a502c75500042d8542920f
Operation Spark <miguelpeter2@gmail.com>
2019-08-13 16:38:22 -0500

    Added badges for playing games

--- db/badgeList.js
+++ db/badgeList.js
@@ -1,18 +1,52 @@
 module.exports = [
+  // ------------Wins Badges-------------
+  {
+    name: '1WinBadge',
+    description: 'Win 1 games of Scatter!',
+    goal: 1,
+    metricId: 1,
+  },
   {
     name: '5WinBadge',
     description: 'Win 5 games of Scatter!',
     goal: 5,
+    metricId: 1,
   },
 
   {
     name: '10WinBadge',
     description: 'Win 10 games of Scatter!',
     goal: 10,
+    metricId: 1,
   },
   {
     name: '15WinBadge',
     description: 'Win 15 games of Scatter!',
     goal: 15,
+    metricId: 1,
+  },
+  // ---------Games Played----------------
+  {
+    name: '1GamePlayed',
+    description: 'Play 1 game of Scatter!',
+    goal: 1,
+    metricId: 2,
+  }, {
+    name: '5GamePlayed',
+    description: 'Play 5 games of Scatter!',
+    goal: 5,
+    metricId: 2,
+  },
+
+  {
+    name: '10GamePlayed',
+    description: 'Play 10 games of Scatter!',
+    goal: 10,
+    metricId: 2,
+  }, {
+    name: '15GamePlayed',
+    description: 'Play 15 games of Scatter!',
+    goal: 15,
+    metricId: 2,
   },
 ];
