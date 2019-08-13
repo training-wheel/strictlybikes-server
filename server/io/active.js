b53097cf13b2a25646d81e004b2823d3ccb46c2f
AS Wilson <alexander.wilson@ncf.edu>
2019-08-12 20:19:11 -0500

    Simplify markerHit and move into constructor

--- server/io/active.js
+++ server/io/active.js
@@ -1,42 +1,60 @@
 const jwt = require('jsonwebtoken');
 const {
-  games, usergames, usermarkers, markers,
+  games, usergames, usermarkers, markers, users,
 } = require('../../db/index').models;
-
-const markerHit = async (data) => {
-  //  create new game
-  try {
-    const { id: markerId, jwt: token } = data;
-    const userId = jwt.verify(token, process.env.JWT_SECRET);
-    const { gameId, code } = await markers.findOne({ where: { markerId } });
-    await usermarkers.create({ userId, markerId, gameId });
-    const userGame = await usergames.findOne({
-      where: {
-        userId,
-        gameId,
-      },
-    });
-    await userGame.increment('markerCount');
-    const { markerCount } = userGame;
-    const { markerLimit } = await games.findByPk(gameId);
-    const { name } = await usergames.findByPk(userId);
-    if (markerCount === markerLimit) {
-      this.socket.emit('end', name);
-      this.socket.to(code).broadcast('end', name);
-    } else {
-      this.socket.to(code).broadcast('hit', name);
-    }
-  } catch (err) {
-    console.error(`Marker hit error: ${err}`);
-  }
-};
 
 class ActiveSocket {
   constructor(socket, server) {
     this.server = server;
     this.socket = socket;
     this.handlers = {
-      createGame: markerHit.bind(this),
+      markerHit: async (data) => {
+        //  create new game
+        try {
+          const { id: markerId, jwt: token } = data;
+          const { id: userId } = jwt.verify(token, process.env.JWT_SECRET);
+          const { gameId } = await markers.findOne({ where: { id: markerId } });
+          await usermarkers.create({ userId, markerId });
+          const userGame = await usergames.findOne({
+            where: {
+              userId,
+              gameId,
+            },
+          });
+          await userGame.increment('markerCount');
+          const { markerCount } = userGame;
+          const game = await games.findOne({
+            where: {
+              id: gameId,
+            },
+            // include: [
+            //   {
+            //     model: markers,
+            //     include: [
+            //       {
+            //         model: usermarkers,
+            //         include: [
+            //           {
+            //             model: users,
+            //           },
+            //         ],
+            //       },
+            //     ],
+            //   },
+            // ],
+          });
+          const { markerLimit, code } = game;
+          const { name } = await users.findByPk(userId);
+          if (markerCount === markerLimit) {
+            this.socket.emit('end', name);
+            this.socket.to(code).emit('end', name);
+          } else {
+            this.socket.to(code).emit('hit', name);
+          }
+        } catch (err) {
+          console.error(`Marker hit error: ${err}`);
+        }
+      },
     };
   }
 }
