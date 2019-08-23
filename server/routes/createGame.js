8382a7e46c6f4de2cbe8d722ffd1dd9528a74683
AS Wilson <alexander.wilson@ncf.edu>
2019-08-22 19:52:49 -0500

    Increment createGame usermetric and check badge

--- server/routes/createGame.js
+++ server/routes/createGame.js
@@ -1,5 +1,5 @@
 const { Router } = require('restify-router');
-const { games, users } = require('../../db/index').models;
+const { games, users, badges, userbadges, metrics, usermetrics } = require('../../db/index').models;
 
 const router = new Router();
 
@@ -9,8 +9,28 @@ const createGame = async (req, res) => {
     const options = req.body;
     options.userId = userId;
     await games.create(options);
-    const user = await users.findByPk(userId);
-    user.increment('gamesCreated');
+    const gamesCreatedMetric = await metrics.findOne({
+      where: {
+        name: 'gamesCreated',
+      },
+    });
+    const [gamesCreatedUserMetric] = await usermetrics.findCreateFind({
+      where: {
+        metricId: gamesCreatedMetric.id,
+        userId,
+      },
+    });
+    await gamesCreatedUserMetric.increment('value');
+    const gamesCreatedBadge = await badges.findOne({
+      where: {
+        metricId: gamesCreatedMetric.id,
+        goal: gamesCreatedUserMetric.value,
+      },
+    });
+    if (gamesCreatedBadge) {
+      const { id: badgeId } = gamesCreatedBadge;
+      userbadges.create({ userId, badgeId });
+    }
     res.send({ userId });
   } catch (err) {
     console.error(`Failed to create game: ${err}`);
