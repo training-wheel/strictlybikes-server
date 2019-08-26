8537eea5bf128873095b7b080fa340c3582e3577
AS Wilson <alexander.wilson@ncf.edu>
2019-08-26 09:23:07 -0500

    Document server listening on the desired port

--- server/index.js
+++ server/index.js
@@ -96,6 +96,10 @@ io.sockets.on('connect', (socket) => {
   });
 });
 
+/**
+ * Begin listening on the desired port
+ */
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('%s listening at %s', server.name, server.url);
