ee24a5ede61b026a5726e0c41d9c0d280252260c
AS Wilson <alexander.wilson@ncf.edu>
2019-08-28 14:38:12 -0500

    Document validateUser middleware

--- server/middleware/validateUser.js
+++ server/middleware/validateUser.js
@@ -1,4 +1,21 @@
+/**
+ * Authentication middleware exported to the server/index
+ */
+
+/**
+ * jwt required to verify user's web token
+ */
+
 const jwt = require('jsonwebtoken');
+
+/**
+ * validateUser is a middleware function to verify users before granting
+ * access to sensitive endpoints
+ * @param {Object} req: The HTTP request object. The web token is attached in
+ * the header. The decrypted user ID is attached to the user property.
+ * @param {Object} res: The HTTP response object.
+ * @param {Function} next: The next callback function to be invoked.
+ */
 
 const validateUser = async (req, res, next) => {
   try {
@@ -11,5 +28,9 @@ const validateUser = async (req, res, next) => {
     res.send(400, `User not signed in: ${err.message}`);
   }
 };
+
+/**
+ * The middleware is exported to server/index
+ */
 
 module.exports = validateUser;
